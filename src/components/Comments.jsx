2e111ff948f2df7c096ee876c287b4c2a21d563e
Ben Bushnell <31861464+VoxideNebula@users.noreply.github.com>
2026-01-29 18:43:49 -0700

    fix comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -6,7 +6,7 @@ const Comments = () => {
     <section className="mt-20 pt-10 border-t border-white/10">
       <Giscus
         id="comments"
-        repo="YOUR_GITHUB_USERNAME/YOUR_REPO_NAME"
+        repo="VoxideNebula/benjaminbushnell.com"
         repoId="R_kgDOL-0V2A"
         category="Announcements"
         categoryId="DIC_kwDOL-0V2M4ClnYV"
